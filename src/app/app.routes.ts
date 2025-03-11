98596b0e4482723781cfbbcbd62ab1ede4c04af8
WhiteYa <nantakornthidee@gmail.com>
2025-03-11 18:21:17 +0700

    change directory and img

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { MainComponent } from './pages/main/main.component';
 import { LocationComponent } from './pages/location/location.component';
 
 export const routes: Routes = [
-    { path: 'Home', component: MainComponent }, 
+    { path: '', component: MainComponent }, 
     { path: 'location', component: LocationComponent }
   ];
