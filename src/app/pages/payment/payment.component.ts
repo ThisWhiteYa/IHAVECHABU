c41e8bf9818e6bae76ae1ebf4ef6c4920f8418e6
Chaiwut <chivwut0646@gmail.com>
2025-03-11 18:48:54 +0700

    payment page

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-payment',
-  imports: [],
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.css'
+  styleUrls: ['./payment.component.scss']
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
+  countdown: number = 15 * 60; // 15 minutes in seconds
+  minutes: number = 15;
+  seconds: number = 0;
+  interval: any;
 
+  ngOnInit() {
+    this.startCountdown();
+  }
+
+  startCountdown() {
+    this.interval = setInterval(() => {
+      if (this.countdown > 0) {
+        this.countdown--;
+        this.minutes = Math.floor(this.countdown / 60);
+        this.seconds = this.countdown % 60;
+      } else {
+        clearInterval(this.interval); // Stop countdown at 0
+        alert("หมดเวลาการชำระเงิน กรุณาทำรายการใหม่!"); // Show timeout alert
+      }
+    }, 1000); // Run every 1 second
+  }
 }
