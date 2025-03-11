b8aa774db79655d1a51544a376500da86e53c41c
Chaiwut <chivwut0646@gmail.com>
2025-03-11 18:25:16 +0700

    payment page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { LocationComponent } from './pages/location/location.component';
+import { PaymentComponent } from './pages/payment/payment.component';
 
 export const routes: Routes = [
     { path: 'Home', component: MainComponent }, 
-    { path: 'location', component: LocationComponent }
+    { path: 'Location', component: LocationComponent },
+    { path: 'Payment', component: PaymentComponent }
   ];
