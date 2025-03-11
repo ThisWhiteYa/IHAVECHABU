a968c71ee2b16f4cce32322e61fff5a865f2e490
WhiteYa <nantakornthidee@gmail.com>
2025-03-12 00:20:32 +0700

    location before payment

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-location',
-  imports: [],
+  imports: [RouterLink,FormsModule],
   templateUrl: './location.component.html',
-  styleUrl: './location.component.css'
+  styleUrl: './location.component.css',
 })
 export class LocationComponent {
+  formData = {
+    name: '',
+    address: '',
+    road: '',
+    subDistrict: '',
+    district: '',
+    province: '',
+    zipCode: '',
+  };
 
+  constructor(private router: Router) {}
+
+  submitForm() {
+    console.log('Form Data Submitted:', this.formData);
+  }
+  onSubmit(form: any) {
+    if (form.valid) {
+      this.router.navigate(['/payment']);
+    } else {
+      alert('กรุณากรอกข้อมูลให้ครบ');
+    }
+  }
 }
