72d7c50f51a8a918010baf09e8a1525a3eb7c282
WhiteYa <nantakornthidee@gmail.com>
2025-03-11 17:08:59 +0700

    delete empty component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink,NavbarComponent],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
